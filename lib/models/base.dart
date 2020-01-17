9711aa8e36c1b4e7c35e6cc349bd67f8f86b82c9
wolf-tungsten <canis.tungsten@foxmail.com>
2020-01-17 08:10:27 +0800

    调整项目结构

--- lib/models/base.dart
+++ lib/models/base.dart
@@ -2,12 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:dio/dio.dart';
 import '../common/config.dart';
 import '../common/store.dart';
+import '../app.dart';
 
-class AppModel extends ChangeNotifier {
-  String apiToken = "";
-  bool isLoggedIn = false;
-
-  
-  
+class WigetBaseModel extends ChangeNotifier {
   
 }
